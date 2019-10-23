64278a89b57a282bc456c2f7502445d5d6e44314
LukaszRachwal <49121795+LukaszRachwal@users.noreply.github.com>
2019-10-23 20:14:19 +0200

    Add files via upload

---
+++ demo/demo/src/test/java/com/example/lukasz/LukaszApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lukasz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukaszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
